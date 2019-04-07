7c537a4a600102a3463dce46432aa9285169e281
eminedenizogu <denzemine@gmail.com>
2019-04-07 13:23:51 +0300

    product abstract class

---
+++ onlineBookStore/ProductClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBookStore
+{
+    abstract class ProductClass
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public double price { get; set; }
+
+        public abstract void printProperties();
+        
+    }
+}
